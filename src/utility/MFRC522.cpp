bc6fac93679ba22c570b2193aa1b38bc15fb4679
Jon Shaw <jonshaw199@gmail.com>
2024-09-04 21:23:33 -0700

    fix uninitialized error

--- src/utility/MFRC522.cpp
+++ src/utility/MFRC522.cpp
@@ -1349,7 +1349,7 @@ void MFRC522::PICC_DumpMifareClassicSectorToSerial(Uid *uid,			///< Pointer to U
 	// The four CX bits are stored together in a nible cx and an inverted nible cx_.
 	uint8_t c1, c2, c3;		// Nibbles
 	uint8_t c1_, c2_, c3_;		// Inverted nibbles
-	bool invertedError;		// True if one of the inverted nibbles did not match
+	bool invertedError = 0;		// True if one of the inverted nibbles did not match
 	uint8_t g[4];				// Access bits for each of the four groups.
 	uint8_t group;				// 0-3 - active group for access bits
 	bool firstInGroup;		// True for the first block dumped in the group
